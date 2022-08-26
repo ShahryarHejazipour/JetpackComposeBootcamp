b48768ceee8d35764a9f099b1db3bbcfd726c4db
Shahryar Hejazipour <shahryar.hejazipour@outlook.com>
2022-08-26 22:13:27 +0430

    Using The Wrapper Class for Complete the repository class - So Beauty

--- app/src/main/java/com/example/jetpackcomposebootcamp/triviaProject/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/jetpackcomposebootcamp/triviaProject/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcomposebootcamp.triviaProject.repository
 
+import android.util.Log
 import com.example.jetpackcomposebootcamp.triviaProject.data.DataOrException
 import com.example.jetpackcomposebootcamp.triviaProject.model.QuestionItem
 import com.example.jetpackcomposebootcamp.triviaProject.network.QuestionAPI
@@ -8,6 +9,23 @@ import javax.inject.Inject
 class QuestionRepository @Inject constructor(private val api: QuestionAPI) {
 
     //Using Wrapper class for adding metadata into the response --- ArrayList<QuestionItem>() ---
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>,Boolean,Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>,Boolean,Exception>()
+
+    suspend fun getAllQuestions():DataOrException<ArrayList<QuestionItem>,Boolean,Exception>{
+
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+
+        }catch (exception:Exception){
+            dataOrException.e = exception
+            Log.d("Exception", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+
+        }
+
+        return dataOrException
+
+    }
 
 }
