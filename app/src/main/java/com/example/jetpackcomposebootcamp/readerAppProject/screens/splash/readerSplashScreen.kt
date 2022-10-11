14978b590c8eb637c6f178bd67b0566dbeb59e15
Shahryar Hejazipour <shahryar.hejazipour@outlook.com>
2022-10-11 12:16:01 +0330

    Adding animation to the splash screen's LOGO and finish the splash screen

--- app/src/main/java/com/example/jetpackcomposebootcamp/readerAppProject/screens/splash/readerSplashScreen.kt
+++ app/src/main/java/com/example/jetpackcomposebootcamp/readerAppProject/screens/splash/readerSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.example.jetpackcomposebootcamp.readerAppProject.screens.splash
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -7,20 +10,49 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
 fun ReaderSplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
-    
+
+    /*
+    For creating the splash screen that means the a LOGO start and after a few seconds it Gone, we need to Use an
+    Compose Animation.
+    For using a Compose animation we have to use  "LaunchedEffect" block code.
+     */
+    val scaleState = remember{
+        androidx.compose.animation.core.Animatable(initialValue = 0f)
+    }
+  LaunchedEffect(key1 = true){
+      scaleState.animateTo(targetValue = 0.9f, animationSpec = tween(900,
+          easing = {
+              OvershootInterpolator(8f).getInterpolation(it)
+
+      }))
+      delay(2000L)
+
+  }
+
+
+
+    /*
+    Creating LOGO for splash screen: All things that in the splash screen ui called LOGO.
+     */
     Surface(modifier = androidx.compose.ui.Modifier
         .padding(15.dp)
-        .size(330.dp),
+        .size(330.dp)
+        .scale(scale = scaleState.value),//For adding animation to the splash screen
         shape = CircleShape,
         border= BorderStroke(width = 2.dp, color = Color.LightGray)
     ) {
